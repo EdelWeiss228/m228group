0de27f15f18806a097262c785b1d3e71a4fe8646
Георгий Таджиев <georgijtadziev@MacBook-Air-Georgij.local>
2024-03-30 12:07:08 +0500

    andijon rep

--- Mem.h
+++ Mem.h
@@ -1,19 +1,27 @@
 #pragma once
 #include "MemoryManager.h"
 
-// Простейший менеджер памяти, использует ::new и ::delete
 class Mem: public MemoryManager
-{
+{    
 public:
+    typedef struct mem_handle_t{
+        int addr;
+        int size;
+        mem_handle_t(int addr, int size) :
+        addr(addr),
+        size(size)
+        {}
+        bool operator==(const mem_handle_t& other) { return other.addr == addr && other.size == size; }
+        bool operator!=(const mem_handle_t& other) { return !operator==(other); }
+    };
     Mem(size_t sz): MemoryManager(sz) {}
-
-    void* allocMem(size_t sz)
-    {
-        return new char[sz];
-    }
-
-    void freeMem(void* ptr)
-    {
-        delete[] ptr;
-    }
+    ~Mem(){}
+    virtual void* allocMem(size_t sz);
+    virtual void freeMem(void* ptr);
+    bool operator=(const int& addr){}
+private:
+    virtual mem_handle_t get_block(int addr, int size);
+    virtual int get_max_block_size();
+    virtual int get_free_space();
+    virtual void print_blocks();
 };
