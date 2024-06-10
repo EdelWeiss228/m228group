e3d9f7fd130befb9af29f9fd2b22ab7ad45db8c0
AnthonyStolpovskiy <antonstolpovskij@gmail.com>
2024-06-10 21:49:52 +0500

    Update List_1.h

--- List_1.h
+++ List_1.h
@@ -6,15 +6,20 @@ class List : public AbstractList
 {
 public:
 	List(MemoryManager& mem) : AbstractList(mem) { list_head = NULL; }
-	~List() { if (list_head) List::clear(); }
+	~List() { 
+		if (list_head) 
+		{
+			List::clear();
+		}
+	}
 
 	int size();
 	size_t max_bytes(); // to be, or not to be?
-	Iterator* find(void* elem, size_t size); 
-	void remove(Iterator* iter);
+	Iterator* find(void* elem, size_t size); // -
+	void remove(Iterator* iter);// -
 
 	int push_front(void* elem, size_t elemSize);
 	void pop_front();
 	void* front(size_t& size);
-	int insert(Iterator* iter, void* elem, size_t elemSize);
+	int insert(Iterator* iter, void* elem, size_t elemSize);// -
 };
